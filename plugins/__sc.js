0d51ec16612f2108751f631f565c911e176db510
JOHAN <105216501+MR19JOHAN@users.noreply.github.com>
2022-06-14 01:58:38 +0100

    Update __sc.js

--- plugins/__sc.js
+++ plugins/__sc.js
@@ -2,6 +2,6 @@ let handler = (t) => {
     t.reply('*~FB~* : *https://www.facebook.com/profile.php?id=100027327995013*')
 }
 
-handler.command = ['Facebook']
+handler.command = ['sc']
 
 export default handler
